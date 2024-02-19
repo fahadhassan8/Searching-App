c63df21a12e5d8cbc385bca4513512282a7ddda6
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-02-19 21:28:24 +0500

    updated

---
+++ app.js
@@ -0,0 +1,220 @@
+var carList = {
+    honda:{
+        city:{
+            model:'2024',
+            seatcapacity:"6 Persons",
+            fuleType: "Petrol",
+            transmission:'Type Automatic',
+            mileage:'20-25 Km/Liter',
+            price:'4,700,000 PKR',
+            color:['white','silver','black']
+        },
+        accord:{
+            model: '2024',
+            seatcapacity:'6 Persons',
+            fuleType:'Petrol',
+            Transmission:'Auto Type',
+            mileage:'16-20 Km/Liter',
+            wheel:'24 Inch',
+            price:'24,000,000 PKR',
+            color:['white','red','black']
+        },
+        HRV:{
+            model:'2023',
+            seatcapacity:'5 Persons',
+            fuleType:'Petrol',
+            transmission:'Auto',
+            mileage:'15-20 Km/Liter',
+            wheel:'17 Inches',
+            price:'8,890,000 PKR',
+            color:['black','red','white',]
+        },
+        BRV:{
+            model:'2024',
+            seatcapacity:'6 Persons',
+            fuleType: 'Petrol',
+            transmission:'auto',
+            mileage:'18-22 Km/Liter',
+            wheel:'20 Inches',
+            price:'9,400,000 PKR',
+            color:['silver','black','white']
+        },
+        civic:{
+            model:'2023',
+            seatcapacity:'4 Persons',
+            fuleType:'Petrol',
+            transmission:'Manual',
+            mileage:'15-20 Km/Liter',
+            wheel:'16 Inches',
+            price:'6,500,000',
+            color:['white','silver','black','blue']
+        }
+    },
+    hyundai:{
+        santafe:{
+            model:'2024',
+            seatcapacity:'4 Persons',
+            fuleType:'Petrol',
+            transmission:'Manual',
+            mileage:'12-16 Km/Liter',
+            wheel:'14 Inches',
+            price:'6,500,000 PKR',
+            color:['red','white','black'],
+        },
+        tucson :{
+            model:'2023',
+            seatcapacity:'5 Person',
+            fuleType : 'Petrol',
+            transmission :'Type	Automatic',
+            mileage: "10 to 14 KM/LITER",
+            wheel :	'14 Inches',
+            price: '8,440,000 PKR',  
+            color: ['red','white','black']   
+        },
+        venue :{
+            model:'2024',
+            seatcapacity:'5 Person',
+            fuleType : 'Petrol',
+            transmission :'Type	Automatic',
+            Mileage: "10 to 14 KM/LITER",
+            Wheel :	'17 Inches',
+            price: '6,850,000 PKR',  
+            color: ['red','white','black']   
+        },
+        kona :{
+            model:'2024',
+            seatcapacity:'4 Person',
+            fuleType : 'Petrol',
+            transmission :'Type	Automatic',
+            Mileage: "10 to 16 KM/LITER",
+            Wheel :	'16 Inches',
+            price: '5,550,000 PKR',  
+            color: ['silver','white','black']   
+        },
+        palisade :{
+            model:'2023',
+            seatcapacity:'5 Person',
+            fuleType : 'Petrol',
+            transmission :'Type	Automatic',
+            Mileage: "12 to 17 KM/LITER",
+            Wheel :	'18 Inches',
+            price: '7,850,000 PKR',  
+            color: ['silver','white','black']   
+        },
+    },
+
+
+}
+toyota : {
+    corolla :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'16 in',
+        price: '5,960,000 PKR',  
+        color: ['red','white','black']   
+    },
+    yaris :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'14 in',
+        price: '4,320,000 PKR',  
+        color: ['red','white','black']   
+    },
+    camry :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'14 in',
+        price: '23,200,000 PKR',  
+        color: ['red','white','black']   
+    },
+    premio :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Automatic',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'14 in',
+        price: '13,200,000 PKR',  
+        color: ['red','white','black']   
+    },
+    landcruiser :{
+        model:'2023',
+        seatcapacity:'8 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "10 to 12 KM/LITER",
+        Wheel :	'19 in',
+        price: '23,200,000 PKR',  
+        color: ['red','white','black']   
+    },
+    crollacross :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "12 to 16 KM/LITER",
+        Wheel :	'16 in',
+        price: '9,399,000 PKR',  
+        color: ['red','white','black']   
+    },
+    landcruiser300 :{
+        model:'2023',
+        seatcapacity:'8 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "115 to 1 KM/LITER",
+        Wheel :	'18 in',
+        price: '33,200,000 PKR',  
+        color: ['red','white','black']   
+    },
+    fortuner :{
+        model:'2023',
+        seatcapacity:'8 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'18 in',
+        price: '14,499,000 PKR',  
+        color: ['red','white','black'],   
+    },
+    revo :{
+        model:'2023',
+        seatcapacity:'5 person',
+        extra : 'loading area',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "10 to 13 KM/LITER",
+        Wheel :	'18 in',
+        price: '11,959,000 PKR',  
+        color: ['red','white','black'],   
+    },
+    hilux :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Auto',
+        Mileage: "15 to 19 KM/LITER",
+        Wheel :	'14 in',
+        price: '3,200,000 PKR',  
+        color: ['red','white','black'],   
+    },
+    hiace :{
+        model:'2023',
+        seatcapacity:'5 person',
+        fuleType : 'petrol',
+        Transmission :'Type	Automatic',
+        Mileage: "10 to 14 KM/LITER",
+        Wheel :	'14 in',
+        price: '13,450,000 PKR',  
+        color: ['red','white','black']   
+    }
+},
